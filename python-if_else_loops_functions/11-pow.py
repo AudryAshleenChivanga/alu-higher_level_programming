247e88bcf925ac867c9ad561486b72c3e97bedc5
Ashleen <a.chivanga@alustudent.com>
2023-07-10 16:59:51 -0700

    Powers

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -3,13 +3,4 @@
 
 
 def pow(a, b):
-    result = 1
-
-    if b >= 0:
-        for _ in range(b):
-            result *= a
-    else:
-        for _ in range(abs(b)):
-            result /= a
-
-    return "{:.18g}".format(result)
+    return (a ** b, 2)
