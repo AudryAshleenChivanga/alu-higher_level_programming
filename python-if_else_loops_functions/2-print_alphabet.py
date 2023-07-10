7538a97ae8a833ef74ec59b036a2262393bd617b
Ashleen <a.chivanga@alustudent.com>
2023-07-10 11:03:16 -0700

    Printing the ASCII alphabet

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 #2-print_alphabet.py
 
+"""Printing the ASCII alphabet in lower case , not followed by a new line."""
 for letter in range(97, 123):
     print("{}".format(chr(letter)), end="")
