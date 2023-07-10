33d02948dbfc23a6dab5044a4118801395204123
Ashleen <a.chivanga@alustudent.com>
2023-07-10 11:24:50 -0700

    Incomplete alphabet

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 # 3-print_alphabt.py
 
 """
@@ -9,6 +8,5 @@ Printing the ASCII alphabet in lowercase, excluding 'q' and 'e', not followed by
 output = ""
 for letter in range(97, 123):
     if chr(letter) not in ['q', 'e']:
-        output += chr(letter)
-
-print(output)  # Print the output without a new line
+        output += "{}".format(chr(letter))
+print("{}".format(output))  # Print the output without a new line
