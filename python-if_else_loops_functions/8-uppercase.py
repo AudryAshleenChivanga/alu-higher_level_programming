37f394462517584b01a6a6d8988216ea92b40ac8
Ashleen <a.chivanga@alustudent.com>
2023-07-10 16:27:12 -0700

    Uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,12 @@
 # Assuming uppercase() is defined in the "uppercase" module
 from uppercase import uppercase
 
-uppercase("best")
-uppercase("Best School 98 Batter street")
+input_str = "best"
+output_str = uppercase(input_str)
+print("Input: '{}'".format(input_str))
+print("Output: '{}'".format(output_str))
+
+input_str = "Best School 98 Batter street"
+output_str = uppercase(input_str)
+print("Input: '{}'".format(input_str))
+print("Output: '{}'".format(output_str))
