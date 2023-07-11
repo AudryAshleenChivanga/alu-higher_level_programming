585354e51e7ea498daf367418eb07c7514c9c080
Ashleen <a.chivanga@alustudent.com>
2023-07-10 17:37:39 -0700

    The Uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
+# For uppercase
 
-# Assuming uppercase() is defined in the "uppercase" module
-from uppercase import uppercase
 
-input_str = "best"
-output_str = uppercase(input_str)
-print("Input: '{}'".format(input_str))
-print("Output: '{}'".format(output_str))
-
-input_str = "Best School 98 Batter street"
-output_str = uppercase(input_str)
-print("Input: '{}'".format(input_str))
-print("Output: '{}'".format(output_str))
+def uppercase(str):
+    for char in str:
+        if ord(char) >= 97 and ord(char) <= 122:
+            char = chr(ord(char) - 32)
+        print(char, end="")
+    print()
