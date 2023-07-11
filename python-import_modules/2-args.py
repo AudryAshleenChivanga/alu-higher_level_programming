156a04f209737fd3ed9c6e821e8315c2b552516a
Ashleen <a.chivanga@alustudent.com>
2023-07-11 00:41:04 -0700

    Corrected file2 changed

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     import sys
-    i = len(sys.argv) - 1  # Get the number of command-line arguments
-
+    i = len(sys.argv) - 1  
     if i == 0:
-        print("{} arguments.".format(i))  # Print the number of arguments when there are none
+        print("{} arguments.".format(i))  
     elif i == 1:
-        print("{} argument:".format(i))  # Print the number of arguments when there is only one
+        print("{} argument:".format(i))  
     else:
-        print("{} arguments:".format(i))  # Print the number of arguments when there are multiple
-
+        print("{} arguments:".format(i))  
     if i >= 1:
         i = 0
         for arg in sys.argv:
             if i != 0:
-                print("{}: {}".format(i, arg))  # Print the index and value of each command-line argument
+                print("{}: {}".format(i, arg))
             i += 1
