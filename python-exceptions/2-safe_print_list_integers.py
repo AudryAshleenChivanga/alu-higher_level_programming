24417ee135f329347a0e2e7039ecf8f92a867948
Ashleen <a.chivanga@alustudent.com>
2023-07-16 01:04:19 -0700

    Exceptions changed

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list, x):
-    count = 0
-    try:
-        for i in range(x):
-            if type(my_list[i]) == int:
-                print("{:d}".format(my_list[i]), end="")
-                count += 1
-        print()
-    except (IndexError, TypeError):
-        pass
-    return count
+def safe_print_list_integers(my_list=[], x=0):
+    funct = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+        except (ValueError, TypeError):
+            pass
+        else:
+            funct += 1
+    print()
+    return (funct)
--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    result = []
+    new_list = []
     for i in range(list_length):
         try:
-            division = 0
-            if i < len(my_list_1) and i < len(my_list_2):
-                if isinstance(my_list_1[i], (int, float)) and isinstance(my_list_2[i], (int, float)):
-                    if my_list_2[i] != 0:
-                        division = my_list_1[i] / my_list_2[i]
-                    else:
-                        print("division by 0")
-                else:
-                    print("wrong type")
-            else:
-                print("out of range")
-            result.append(division)
+            result = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
             print("division by 0")
-            result.append(division)
-    return result
+            result = 0
+        except TypeError:
+            print("wrong type")
+            result = 0
+        except IndexError:
+            print("out of range")
+            result = 0
+        finally:
+            new_list.append(result)
+
+    return (new_list)
