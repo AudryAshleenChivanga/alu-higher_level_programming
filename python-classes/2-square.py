d7ceab810fde47e52f656f9bf3f4a3ae1fcf0cf2
Ashleen <a.chivanga@alustudent.com>
2023-07-24 09:25:10 -0700

    class Square that defines a square by: (based on 1-square.py) changed

---
+++ python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Defining a square based on 1-square."""
+
+class Square:
+    """Representing the square."""
+
+    def __init__(self, size=0):
+        """Initializing a new square.
+        Args:
+        size (int): The size of the new Square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("Size must be an integer")
+        elif size < 0:
+            raise ValueError("Size must be >= 0")
+        self.__size = size
