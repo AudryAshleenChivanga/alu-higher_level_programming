5ef0686397f4a2ddd0782514e01775d8b661b70d
Ashleen <a.chivanga@alustudent.com>
2023-07-27 15:10:16 -0700

    file1 changed

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""A list that inhert the list object"""
+""" Program that sort a list int """
 
 
 class MyList(list):
-    """Represent a Mylist"""
-
+    """ class that inherist from list """
     def print_sorted(self):
-        '''
-        prints the list, but sorted
-        '''
-        print(sorted(self))
+        """ prints the list, but sorted (ascending sort) """
+        new_list = self[:]
+        new_list.sort()
+        print(new_list)
