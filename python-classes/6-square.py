6d9957d179b7e25f5edc5e3f076af86983575176
Ashleen <a.chivanga@alustudent.com>
2023-07-27 04:22:03 -0700

    File6 changed

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
-"""Defining the a square"""
+"""Define a square"""
 
 
 class Square:
-    """Creates a square.
- The instance attributes: size
+    """
+    Creates a square.
     """
 
     def __init__(self, size=0, position=(0, 0)):
-        """Initialize data."""
+        """Initialize variables."""
         self.__size = size
         self.__position = position
 
     @property
     def size(self):
-        """Retrieve size."""
+        """returns size."""
         return self.__size
 
     @size.setter
-    def size(self, value):
-        """Sets size to a value."""
-        if not isinstance(value, int):
+    def size(self, size):
+        """size to a size."""
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        self.__size = size
 
-    def _init_(self, size=0, position=(0, 0)):
+    def __init__(self, size=0, position=(0, 0)):
         """Initializes the data."""
         self.size = size
         self.position = position
