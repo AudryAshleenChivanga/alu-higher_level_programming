1a011998a7d11da10881b20eae3ca961dae2bb9e
Ashleen <a.chivanga@alustudent.com>
2023-07-28 03:07:00 -0700

    advanced101 changed

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -1,10 +1,54 @@
 #!/usr/bin/python3
-"""A module with one function."""
+# script that reads stdin line by line and computes metrics
+"""
+    define a function 'print_stats'
+"""
 
 
-def add_attribute(ob, attr, value):
-    """adding attribute to class"""
-    if hasattr(ob, "__dict__"):
-        setattr(ob, attr, value)
-    else:
-        raise TypeError("can't add new attribute")
+def print_stats(size, status_codes):
+    """
+        print accumulated metrics
+    """
+
+    print("File size: {}".format(size))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
+
+
+if __name__ == "__main__":
+    import sys
+
+    size = 0
+    status_codes = {}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    count = 0
+
+    try:
+        for line in sys.stdin:
+            if count == 10:
+                print_stats(size, status_codes)
+                count = 1
+            else:
+                count += 1
+
+            line = line.split()
+
+            try:
+                size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
+
+            try:
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
+
+        print_stats(size, status_codes)
+
+    except KeyboardInterrupt:
+        print_stats(size, status_codes)
+        raise
