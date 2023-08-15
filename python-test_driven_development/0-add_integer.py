0eaefdbd698f89bdcaefa49a83999f238895d1d6
Ashleen <a.chivanga@alustudent.com>
2023-08-15 06:04:33 -0700

    O-adding integr changed

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -7,9 +7,9 @@ This module supplies one function.
 def add_integer(a, b):
     """The addition of 2 numbers."""
     if type(a) is not int and type(a) is not float:
-        raise TypeError("a must be an integer")
+        raise TypeError("a must be an integer or float")
     if type(b) is not int and type(b) is not float:
-        raise TypeError("b must be an integer")
+        raise TypeError("b must be an integer or float")
     if type(a) is float:
         a = int(a)
     if type(b) is float:
