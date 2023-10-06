d76e737941134cffc34c19e129b18876620ee10e
Ashleen Chivanga <127235974+AudryAshleenChivanga@users.noreply.github.com>
2023-10-06 21:29:09 +0000

    Header updated

---
+++ javascript-web_jquery/0-script.js
@@ -0,0 +1,5 @@
+// Using document.querySelector to select the <header> tag
+let headerElement = document.querySelector('header');
+
+// Updating the text color to red (#FF0000)
+headerElement.style.color = '#FF0000';
