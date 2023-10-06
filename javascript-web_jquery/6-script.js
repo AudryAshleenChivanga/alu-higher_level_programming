dbe9fa70f215b27235e9857a11020de7203844ff
Ashleen Chivanga <127235974+AudryAshleenChivanga@users.noreply.github.com>
2023-10-06 22:05:47 +0000

    pdates the text of the <header> element to New Header!!!

---
+++ javascript-web_jquery/6-script.js
@@ -0,0 +1,5 @@
+// Using jQuery to listen for a click event on the DIV#update_header
+$('#update_header').click(function() {
+    // Updating the text of the <header> element
+    $('header').text('New Header!!!');
+});
