41b85d41f3e74aeab5b42b64edc7e61c62e56424
Ashleen Chivanga <127235974+AudryAshleenChivanga@users.noreply.github.com>
2023-10-06 21:51:14 +0000

    File 3 updated

---
+++ javascript-web_jquery/3-script.js
@@ -0,0 +1,5 @@
+// I am using jQuery to listen for a click event on the DIV#red_header
+$('#red_header').click(function() {
+    // When clicked, add the class 'red' to the <header>
+    $('header').addClass('red');
+});
