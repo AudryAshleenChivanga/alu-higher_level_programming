52afb5f7a14f8ca8a81846c91a5d1660b2e7033c
Ashleen Chivanga <127235974+AudryAshleenChivanga@users.noreply.github.com>
2023-10-06 22:11:20 +0000

    fetches the character name

---
+++ javascript-web_jquery/7-script.js
@@ -0,0 +1,5 @@
+// Use jQuery's get method to fetch data from the provided URL
+$.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function(data) {
+    // Update the content of the DIV#character with the fetched name
+    $('#character').text(data.name);
+});
